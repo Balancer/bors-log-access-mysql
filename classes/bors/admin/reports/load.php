bf13ea2aaee45de9bba916b20b10580cda8c4fbc
Balancer <balancer@balancer.ru>
2009-07-17 00:25:31 +0400

    Наконец, вынос анализа ботов в отдельную функцию.

--- classes/bors/admin/reports/load.php
+++ classes/bors/admin/reports/load.php
@@ -4,6 +4,7 @@ class bors_admin_reports_load extends base_page
 {
 	function title() { return ec('Загрузка системы'); }
 	function config_class() { return config('admin_config_class'); }
+	function template() { return 'forum/_header.html'; }
 
 	function local_data()
 	{
@@ -25,6 +26,18 @@ class bors_admin_reports_load extends base_page
 					'limit' => 20,
 				)
 			),
+
+			'max_cpu_by_combine' => $dbh->select_array('bors_access_log',
+				'user_ip, class_name, user_id, count(*) as cnt, sum(operation_time) as su, is_bot, user_agent',
+				array('group'=>'user_ip,class_name',
+					'order' => '-su',
+					'limit' => 20,
+				)
+			),
+
+			'can_see_ip' => bors()->user() ? !!bors()->user()->is_coordinator() : false,
 		);
 	}
+
+	function cache_static() { return rand(60, 120); }
 }
