94ec902289617b5957c13f82e37de86888f95a33
Balancer <balancer@balancer.ru>
2010-07-26 03:37:14 +0400

    ++

--- tools/cron/access_log_counting.php
+++ tools/cron/access_log_counting.php
@@ -3,6 +3,8 @@
 require_once('../config.php');
 include_once(BORS_CORE.'/init.php');
 
+echo "---[ access log counting ]---\n";
+
 $db = new driver_mysql(config('bors_core_db'));
 $db->query('DELETE FROM bors_access_log WHERE access_time < UNIX_TIMESTAMP() - 3600');
 
@@ -23,4 +25,5 @@ foreach(objects_array('bors_access_log', array('was_counted' => 0)) as $x)
 	}
 }
 
+echo "\n";
 bors_exit();
