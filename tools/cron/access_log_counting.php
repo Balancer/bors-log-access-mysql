a49338de868caa250e0199e8898fd79d82500910
Balancer <balancer@balancer.ru>
2010-12-26 17:13:42 +0300

    back

--- tools/cron/access_log_counting.php
+++ tools/cron/access_log_counting.php
@@ -22,14 +22,14 @@ foreach(objects_array('bors_access_log', array('was_counted' => 0)) as $x)
 {
 	if(!$x->is_bot() && $target = $x->target())
 	{
-		bors_external_referer::register($x->server_uri(), $x->referer(), $target);
-		$target->visits_inc();
+#		bors_external_referer::register($x->server_uri(), $x->referer(), $target);
+#		$target->visits_inc();
 		$x->set_was_counted(1, true);
 		echo "+";
 	}
 	else
 	{
-		bors_external_referer::register($x->server_uri(), $x->referer(), NULL);
+#		bors_external_referer::register($x->server_uri(), $x->referer(), NULL);
 		$x->set_was_counted(2, true);
 		echo ".";
 	}
