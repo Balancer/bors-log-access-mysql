38d63f6edeb75a3d70372d2b5329faa81eade5c3
Balancer <balancer@balancer.ru>
2011-01-31 02:57:42 +0300

    Исправления и доработки.

--- classes/bors/access/log.php
+++ classes/bors/access/log.php
@@ -26,6 +26,6 @@ class bors_access_log extends base_object_db
 		);
 	}
 
-	function auto_targets()	{ return array('target' => 'target_class_name(target_object_id)'); }
+	function auto_targets()	{ return array_merge(parent::auto_targets(), array('target' => 'target_class_name(target_object_id)')); }
 	function auto_objects()	{ return array('user' => 'bors_user(user_id)'); }
 }
