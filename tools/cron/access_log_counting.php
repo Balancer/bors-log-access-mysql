8ed51437e712698714b0246c2e608b9531e70936
Balancer <balancer@balancer.ru>
2016-04-15 06:13:30 +0300

    ++

--- tools/cron/access_log_counting.php
+++ tools/cron/access_log_counting.php
@@ -16,10 +16,16 @@ catch(Exception $e)
 	exit();
 }
 
-$db->query('DELETE FROM bors_access_log WHERE access_time < UNIX_TIMESTAMP() - 600');
+$db->query('DELETE LOW_PRIORITY FROM bors_access_log WHERE access_time < '.(time() - 600));
 
 foreach(bors_find_all('bors_access_log', array('was_counted' => 0)) as $x)
 {
+	if(in_array($x->target_class_name(), ['bors_tools_ajax_module', 'bors_system_js_touch', 'bors_cg_f2f_css_less', 'bors_data_lists_json']))
+		continue;
+
+//	if(preg_match($x->referer())
+//		continue;
+
 	if(!$x->is_bot() && ($target = $x->target()))
 	{
 		try {
