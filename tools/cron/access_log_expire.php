933d0c44fc8f995f6bc5aeb7fd39d77c0f103fec
Balancer <balancer@balancer.ru>
2016-05-08 14:02:52 +0300

    accumulated

--- tools/cron/access_log_expire.php
+++ tools/cron/access_log_expire.php
@@ -15,8 +15,8 @@ bors_exit();
 function main()
 {
 	$dbh = new driver_mysql(config('bors_local_db'));
-	$dbh->delete('bors_access_log', array(
+	$dbh->delete('bors_access_log', [
+		'*priority' => 'low',
 		'access_time<' => time()-600,
-//		'limit' => 100000,
-	));
+	]);
 }
